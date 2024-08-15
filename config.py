280f2a25e6a7592084e0c3b833e3e18f70c60bfb
xdevilxyz <129171425+xdevilxyz@users.noreply.github.com>
2024-08-15 08:47:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,18 +14,18 @@ API_HASH = os.environ.get("API_HASH", "f317b3f7bbe390346d8b46868cff0de8")
 OWNER = os.environ.get("OWNER", "i_am_yamraj") #Owner username
 OWNER_ID = int(os.environ.get("OWNER_ID", "1837294444")) #Owner user id
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluser10")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluserhh")
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002004278204")) #database save channel id 
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002049574995"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002097822007"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001940737153"))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002134495794"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001980994910"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002106690102"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001567792175"))
+FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002121257524"))
 
 #Shortner (token system) 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "adrinolinks.in") 
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "5883bbd80da73ef7908bf7922c3aa8050586588d")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com") 
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "8960075d61d126996682b45d5d8b4503a8d5e717")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/HTDTeraBox/6")
@@ -41,7 +41,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link. Owner @NeonGhost_Networks")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link. Owner @NeonGhost_Networks \n\n Join For Newly Release MOvies @new_movies_2024_hindi")
 
 try:
     ADMINS=[6020516635]
@@ -51,11 +51,11 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>\n After Joining All channels Please click on Try Again ")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my all Channel/Group to use me\n\nKindly Please join Channel</b>\n After Joining All channels Please click on Try Again ")
 
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "@NeonGhost_Networks") # remove None and fo this ->: "here come your txt" also with this " " 
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "@NeonGhost_Network") # remove None and fo this ->: "here come your txt" also with this " " 
 
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
